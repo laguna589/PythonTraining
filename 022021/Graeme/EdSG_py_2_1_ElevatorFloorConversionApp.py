cd3503bb08ec40f219c9866e6eebdd8be7f89032
Edgar Sánchez Grajeda <EdSG@users.noreply.github.com>
2021-02-26 22:49:09 -0800

    Edgar: Changed !bang to use python3.

--- 022021/Graeme/EdSG_py_2_1_ElevatorFloorConversionApp.py
+++ 022021/Graeme/EdSG_py_2_1_ElevatorFloorConversionApp.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python
+#!/usr/local/bin/python3
 
-inp = input('Europe floor? ')
+inp = input("Europe floor? ")
 
 usf = int(inp) + 1 # US floors start at 1 for Ground Floor
 
-print("US floor: ", usf)
+print("US floor: " + str(usf))
 
