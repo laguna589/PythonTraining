233e2b1440d8583ce0b8b4a88e0625b00e7b9ed5
Kavya <65570926+kavyanataraj-dev@users.noreply.github.com>
2021-03-01 23:10:03 -0800

    Create kavyanataraj-dev_py_03_CtoF.py
    Write a program which prompts the user for a Celsius temperature, convert the temperature to Fahrenheit, and print out the converted temperature

---
+++ 022021/Mo/Kavya/kavyanataraj-dev_py_03_CtoF.py
@@ -0,0 +1,5 @@
+#Exercise 5: Write a program which prompts the user for a Celsius temperature, convert the temperature to Fahrenheit, and print out the converted temperature.
+
+Celsius = int(input("Enter a temp in Celsius(integers only): ")) # input() is used take user input & int is used so that Celsius value can be multiplied by 9/5
+Farenheit = ((9/5)*Celsius)+32 #formula used for converting celsius to farneheit
+print("Temp in Farenheit", Farenheit) #final output
