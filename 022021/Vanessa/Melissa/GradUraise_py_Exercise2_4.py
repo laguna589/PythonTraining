4f0d42c56311f23c5ad9473b8bb8966eb2c1c674
Melissa Roetker <54007114+GradUraise@users.noreply.github.com>
2021-03-02 08:39:49 -0700

    Create GradUraise_py_Exercise2_4.py
    Created Exercise 2.4 programming

---
+++ 022021/Vanessa/Melissa/GradUraise_py_Exercise2_4.py
@@ -0,0 +1,21 @@
+# Exercise 2.4
+w = 17
+h = 12.0
+w1 = w//2
+w2 = w/2.0
+h1 = h/3
+v1 = 1 + 2 * 5
+print('Width is equal to:', w)
+print('Height is equal to:', h)
+print('Value of Width//2 is:', w1)
+print('This expression type is:', type(w1))
+print()
+print('Value of Width/2.0 is:', w2)
+print('This expression type is:', type(w2))
+print()
+print('Value of Height/3 is:', h1)
+print('This expression type is:', type(h1))
+print()
+print('The value of 1 + 2 * 5 is:', v1)
+print('This expression type is:', type(v1))
+print()
