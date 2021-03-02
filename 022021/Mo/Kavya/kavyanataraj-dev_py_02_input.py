39e5d4534f5d00538d12399c60134eaba2366a13
Kavya <65570926+kavyanataraj-dev@users.noreply.github.com>
2021-03-01 22:47:14 -0800

    Create kavyanataraj-dev_py_02_input.py
    Exercise: Write a program that uses input to prompt a user for their name and then welcomes them

---
+++ 022021/Mo/Kavya/kavyanataraj-dev_py_02_input.py
@@ -0,0 +1,4 @@
+# Exercise: Write a program that uses input to prompt a user for their name and then welcomes them
+
+name = input()  # Used a mnemonic variable name and input() is used for taking user input
+print("Welcome", name)  # prints "Welcome" statement with a name provided by the user
