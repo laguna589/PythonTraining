0e13b1d1704e8dfddde2aedd84f8dce6b9dfc8a0
Melissa Roetker <54007114+GradUraise@users.noreply.github.com>
2021-03-03 09:48:04 -0700

    Update GradUraise_py_Exercise4_6.py
    Added try/except to verify values are numeric

--- 022021/Vanessa/Melissa/GradUraise_py_Exercise4_6.py
+++ 022021/Vanessa/Melissa/GradUraise_py_Exercise4_6.py
@@ -1,11 +1,28 @@
-def computepay (hours, rate):
-    if float(hours)<= 40 :
-        pay = int(hours) * float(rate)
+# Exercise 4.6
+def computepay (hval, rval):
+    if hval <= 40 :
+        pay = hval * rval
     else :
-        pay = (40 * float(rate) + ((int(hours) - 40) * float(rate) * 1.5))
+        pay = (40 * rval + ((hval - 40) * rval * 1.5))
     print ('Pay:' , pay)
 
 hours = input('Enter Hours: ')
+try:
+    hval = float(hours)
+except:
+    hval = -1
 rate = input('Enter Rate: ')
-computepay (hours, rate)
+try:
+    rval = float(rate)
+except:
+    rval = -1
+
+if hval < 0 and rval < 0:
+    print('Error - Hours and Rates are not valid numbers; Please enter numeric values')
+elif hval < 0:
+    print('Error - Hours is not a valid number; Please enter a numeric value')
+elif rval < 0:
+    print('Error - Rates is not a valid number; Please enter a numeric value')
+else:
+    computepay (hval, rval)
 print()
