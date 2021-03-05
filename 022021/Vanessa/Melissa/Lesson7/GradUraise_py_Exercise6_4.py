66491d3630788c5e6667689b9eeaf4c6e4ff6cb2
Melissa Roetker <54007114+GradUraise@users.noreply.github.com>
2021-03-05 09:39:12 -0700

    Update GradUraise_py_Exercise6_4.py

--- 022021/Vanessa/Melissa/Lesson7/GradUraise_py_Exercise6_4.py
+++ 022021/Vanessa/Melissa/Lesson7/GradUraise_py_Exercise6_4.py
@@ -4,3 +4,8 @@ search = input('Enter a lower case letter in the word to count: ')
 
 print(word.count(search))
 print()
+
+word = 'banana'
+print('New word is banana. How many times is the letter a in banana?')
+print(word.count('a'))
+print()
