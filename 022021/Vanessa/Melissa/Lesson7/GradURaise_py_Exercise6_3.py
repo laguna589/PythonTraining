c03c743d25e43cca9a178e333b78f729758f313a
Melissa Roetker <54007114+GradUraise@users.noreply.github.com>
2021-03-05 09:22:03 -0700

    Update GradURaise_py_Exercise6_3.py

--- 022021/Vanessa/Melissa/Lesson7/GradURaise_py_Exercise6_3.py
+++ 022021/Vanessa/Melissa/Lesson7/GradURaise_py_Exercise6_3.py
@@ -1,8 +1,14 @@
 # Exercise 6.3
+# Exercise 6.2
 word = input('Enter a lower case word: ')
 search = input('Enter a lower case letter in the word to count: ')
-count = 0
-for letter in word:
-    if letter == search:
-        count = count + 1
-print(count)
+
+def count(word,search):
+    num = 0
+    for letter in word:
+        if letter == search:
+            num = num + 1
+
+            print(num)
+count(word,search)
+print()
