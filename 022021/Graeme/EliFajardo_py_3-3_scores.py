95458402e46952d28a163ef03c62230e1ea4e9a3
EliFajardoB <73150314+EliFajardoB@users.noreply.github.com>
2021-03-06 18:51:38 -0600

    Add files via upload

---
+++ 022021/Graeme/EliFajardo_py_3-3_scores.py
@@ -0,0 +1,24 @@
+print ("::: EASY GRADES :::")
+score = input ("Enter score: ")
+try:
+    score = float(score)
+except:
+    score = -1 
+    print ("Try input numbers")
+    exit(score)
+
+if score >= 0 and score <= 1:
+        if score >= 0.9:
+            print ("A")
+        elif score >= 0.8:
+            print ("B")
+        elif score >= 0.7:
+            print ("C")
+        elif score >= 0.6:
+            print ("D")
+        elif score < 0.6:
+            print ("F")
+else:
+    print ("Score is out of range, try again")
+
+    
